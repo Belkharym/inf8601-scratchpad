7d6b57253ba6fe355a7a69caf34d55d9be8ac49f
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2019-09-08 17:49:50 -0400

    Fixed 04-pthread-max

--- 04-pthread-max/main.cpp
+++ 04-pthread-max/main.cpp
@@ -1,15 +1,29 @@
-#include <QDebug>
+#include <vector>
+#include <iostream>
 #include <pthread.h>
+#include <semaphore.h>
 
 pthread_cond_t cond;
-pthread_mutex_t mutex;
+pthread_mutex_t cond_mutex;
+int thread_counter = 0;
+pthread_mutex_t counter_mutex;
+sem_t broadcast_sem;
+bool ready = false;
 
 void *bidon(void *arg)
 {
     (void) arg;
-    pthread_mutex_lock(&mutex);
-    pthread_cond_wait(&cond, &mutex);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&counter_mutex);
+    --thread_counter;
+    if (thread_counter <= 0) {
+        sem_post(&broadcast_sem);
+    }
+    pthread_mutex_unlock(&counter_mutex);
+    pthread_mutex_lock(&cond_mutex);
+    while(!ready) {
+        pthread_cond_wait(&cond, &cond_mutex);
+    }
+    pthread_mutex_unlock(&cond_mutex);
     return 0;
 }
 
@@ -18,26 +32,39 @@ int main(int argc, char *argv[])
     (void) argc; (void) argv;
 
     pthread_cond_init(&cond, NULL);
-    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&cond_mutex, NULL);
+    pthread_mutex_init(&counter_mutex, NULL);
+    sem_init(&broadcast_sem, 0, 0);
 
-    QVector<pthread_t> threads;
+    std::vector<pthread_t> threads;
 
+    pthread_mutex_lock(&counter_mutex);
     for (;;) {
         pthread_t t;
         int ret = pthread_create(&t, NULL, bidon, NULL);
         if (ret == 0) {
-            threads.append(t);
+            threads.push_back(t);
         } else {
-            qDebug() << "max thread" << threads.size();
+            std::cout << "max thread" << threads.size();
             break;
         }
     }
+    thread_counter = threads.size();
+    pthread_mutex_unlock(&counter_mutex);
 
-    pthread_mutex_lock(&mutex);
+    sem_wait(&broadcast_sem);
+
+    pthread_mutex_lock(&cond_mutex);
+    ready = true;
     pthread_cond_broadcast(&cond);
-    pthread_mutex_unlock(&mutex);
-    foreach (pthread_t thread, threads) {
+    pthread_mutex_unlock(&cond_mutex);
+    for (pthread_t thread : threads) {
         pthread_join(thread, NULL);
     }
+
+    pthread_mutex_destroy(&cond_mutex);
+    pthread_mutex_destroy(&counter_mutex);
+    sem_destroy(&broadcast_sem);
+    pthread_cond_destroy(&cond);
     return 0;
 }
